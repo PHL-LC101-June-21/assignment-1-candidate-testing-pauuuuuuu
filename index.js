bffecdaa523cae13bdea809b2c19964dd0431a7e
Paula Sanchez <41597298+pauuuuuuu@users.noreply.github.com>
2021-06-27 05:58:47 +0000

    2.2 works, starting 2.3 line43

--- index.js
+++ index.js
@@ -6,10 +6,21 @@ const input = require('readline-sync');
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
-let candidateAnswer = "";
+let candidateAnswer = " ";
 let questions;
 let correctAnswers;
-let candidateAnswers;
+let candidateAnswers = " ";
+
+questions = ["Who was the first American woman in space? ",	
+"True or false: 5 kilometer == 5000 meters? ",	
+"(5 + 3)/2 * 10 = ? ",	 
+"Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", 	
+"What is the minimum crew size for the ISS? "	
+];
+correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
+
+
+
 
 //function askForName() {}
 // TODO 1.1b: Ask for candidate's name 
@@ -18,18 +29,26 @@ let candidateAnswers;
 
 //function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer// 
+  //Replace your code from TODO 1.2b with a loop that programmatically asks each question in the array and stores the user's responses.//
   let answerQuestion = input.question(question);
   console.log(); 
 
+  for (let i = 0; i < 5; i++){
+    let answerQuestions = input.question(questions[i]);
+    correctAnswers.push(answerQuestions);
+  }
+  console.log(correctAnswers);
 //function gradeQuiz(candidateAnswers) {//
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly //
- 
+// Replace the basic feedback from TODO 1.2c with a template literal that displays each of the candidate's responses in addition to the corresponding correct answers.//
   if(correctAnswer.toLowerCase() == answerQuestion.toLowerCase()) {
     console.log('Good job, your answer is correct!');
    
    } else {
      console.log("Sorry, it is not correct.");
    }
+
+
   
 //let grade;
   
